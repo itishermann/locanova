8b3c956f0fae4de152b4ac0c5d0fb358e9cf075d
Hermann Kao <38349706+itishermann@users.noreply.github.com>
2024-12-20 11:55:55 +0100

    chore:
    add markdown support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+	// Add markdown plugins here, as desired
+	options: {
+		remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter],
+		rehypePlugins: [],
+	},
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
--- src/providers/theme.provider.tsx
+++ src/providers/theme.provider.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ComponentProps } from "react";
+import * as React from "react";
+const NextThemesProvider = dynamic(
+	() => import("next-themes").then((e) => e.ThemeProvider),
+	{
+		ssr: false,
+	},
+);
 
-export function ThemeProvider({
-	children,
-	...props
-}: ComponentProps<typeof NextThemesProvider>) {
+// @ts-expect-error - This fixes the hydratation warning issue @see https://github.com/shadcn-ui/ui/issues/5552
+import type { ThemeProviderProps } from "next-themes/dist/types";
+import dynamic from "next/dynamic";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
